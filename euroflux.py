87859c3b2bcac7feed73ee65b514967dcaf1316f
James <jsimkins2@wisc.edu>
2016-04-27 16:37:27 -0500

    subtracting lists

--- euroflux.py
+++ euroflux.py
@@ -6,6 +6,7 @@ Created on Tue Apr 26 17:34:12 2016
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 def getColumns(inFile, delim="\t", header=True):
     """
     Get columns of data from inFile. The order of the rows is respected
@@ -44,20 +45,119 @@ def getColumns(inFile, delim="\t", header=True):
                 i += 1
                 
     return cols, name
-year = (2006,2008,2010,2011,2012)
+year = (2006,2007,2008,2010,2011,2012)
 year = map(int, year)
+
+neco2  = []
+netime = []
+nefc = []
+newd = []
+news = []
+neta = []
+
+
+# have to convert to float before you can run np.nan
+for i in year:
+    f = file('EFDC_L2_Flx_ATNeu_'+str(i)+'_v02_30m.txt', 'r')
+    cols, name = getColumns(f, delim=",")
+    #define each variable column
+    c = cols['CO2']
+    c = np.array(c)
+    c = c.astype('float')
+    c[c == -9999] = np.nan
+    neco2.append(c)
+    
+    time = cols['DTime']
+    time = np.array(time)
+    time = time.astype('float')
+    time[time == -9999] = np.nan
+    netime.append(time)
+    
+    fc = cols['FC']
+    fc = np.array(fc)
+    fc = fc.astype('float')
+    fc[fc == -9999] = np.nan
+    nefc.append(fc)
+    
+    wd = cols['WD']
+    wd = np.array(wd)
+    wd = wd.astype('float')
+    wd[wd == -9999] = np.nan
+    newd.append(wd)
+    
+    ws = cols['WS']
+    ws = np.array(ws)
+    ws = ws.astype('float')
+    ws[ws == -9999] = np.nan
+    news.append(ws)
+    
+    ta = cols['TA']
+    ta = np.array(ta)
+    ta = ta.astype('float')
+    ta[ta == -9999] = np.nan
+    neta.append(ta)   
+f.close()
+
 chco2  = []
+chtime = []
+chfc = []
+chwd = []
+chws = []
+chta = []
 
 
 # have to convert to float before you can run np.nan
 for i in year:
     f = file('EFDC_L2_Flx_CHCha_'+str(i)+'_v02_30m.txt', 'r')
     cols, name = getColumns(f, delim=",")
+    #define each variable column
     c = cols['CO2']
     c = np.array(c)
     c = c.astype('float')
     c[c == -9999] = np.nan
     chco2.append(c)
+    
+    time = cols['DTime']
+    time = np.array(time)
+    time = time.astype('float')
+    time[time == -9999] = np.nan
+    chtime.append(time)
+    
+    fc = cols['FC']
+    fc = np.array(fc)
+    fc = fc.astype('float')
+    fc[fc == -9999] = np.nan
+    chfc.append(fc)
+    
+    wd = cols['WD']
+    wd = np.array(wd)
+    wd = wd.astype('float')
+    wd[wd == -9999] = np.nan
+    chwd.append(wd)
+    
+    ws = cols['WS']
+    ws = np.array(ws)
+    ws = ws.astype('float')
+    ws[ws == -9999] = np.nan
+    chws.append(ws)
+    
+    ta = cols['TA']
+    ta = np.array(ta)
+    ta = ta.astype('float')
+    ta[ta == -9999] = np.nan
+    chta.append(ta)   
+f.close()
+### Time for plotting ###
+
+chwd = list(itertools.chain(*chwd))
+chco2 = list(itertools.chain(*chco2))
+neco2 = list(itertools.chain(*neco2))
+    
+# x1 - x2
+#np.subtract(x1, x2)
+
+
+
     
 '''
 for n,i in enumerate(chco2[0]):
